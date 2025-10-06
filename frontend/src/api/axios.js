68f0d06ea25e3034dfff2b935366f1d7c161f79d
lentakristina <lentakristina512@gmail.com>
2025-10-06 11:49:43 +0700

    Use environment variable for API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,11 +2,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-   baseURL: 'http://127.0.0.1:8000/api',
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-  },
+   baseURL: process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api',
+   headers: {
+     'Content-Type': 'application/json',
+     'Accept': 'application/json',
+   },
 });
 
 // Request interceptor - adds token to all requests
